b96370a24679194612a26544d5739b4c71e44075
Christopher Oka <70914858+ChristopherOka@users.noreply.github.com>
2022-07-27 22:37:40 -0700

    Proof of concept form field components... still learning how to do this

--- pages/form.js
+++ pages/form.js
@@ -1,6 +1,11 @@
 import Link from 'next/link'
-import PrimaryButton from '../components/primaryButton'
+import Button from '../components/button'
+import FormField from '../components/formField'
 import styles from '../styles/Form.module.css'
+
+function submitForm() {
+
+}
 
 export default function Form() {
     return (
@@ -11,9 +16,17 @@ export default function Form() {
                     <a>Home</a>
                 </Link>
             </h2>
-            <h2 className={styles.subtitle}>Name</h2>
-            <input type="text" className={styles.input} />
-            <PrimaryButton clickHandler={() => {console.log('test')}}>Submit</PrimaryButton>
+            <form>
+                <div style={{display: 'flex', flexDirection: 'column'}}>
+                    <FormField type="input" text="Name" placeholder="Enter your name" required="true"/>
+                    <FormField type="input"  text="Email" placeholder="Enter your email" required="true"/>
+                    <FormField type="input" text="Phone" placeholder="Enter your phone" required="true"/>
+                    <FormField type="select" options={['Mississauga', 'Burlington']} text="City" placeholder="Enter your city" required="true"/>
+                    <FormField type="input" text="Address" placeholder="Enter your address" required="true"/>
+                    <FormField type="input" text="Additional Information" placeholder="Enter your any additional information" />
+                    <Button type="primary" clickHandler={submitForm}>Submit</Button>
+                </div>
+            </form>
         </div>
     )
 }
