4229519c60e64fd9f3f2e21862344290b95e952b
Christopher Oka <70914858+ChristopherOka@users.noreply.github.com>
2022-07-28 19:44:16 -0700

    setup supabase, pulled products from database, and extracted form data in the front end

--- components/formField.js
+++ components/formField.js
@@ -3,18 +3,18 @@ import styles from '../styles/formField.module.css'
 export default function FormField (props) {
     let formField;
     if (props.type === 'input') {
-        formField = <input className={styles.input} type="text" placeholder={props.placeholder} required={props.required}/>
+        formField = <input onChange={props.onChange} className={styles.input} type="text" placeholder={props.placeholder} name={props.name} required={props.required}/>
     } else if (props.type === 'select') {
-        formField = <select className={styles.select}>
-            <option value="">- select an option -</option>
+        formField = <select onChange={props.onChange} className={styles.select}>
+            <option name={props.name} value="">- select an option -</option>
             {props.options.map(option =>  <option key={option}>{option}</option>)}
         </select>
     }
     return (
-        <>
+        <div className={styles.formField}>
             <label className={styles.label}>{props.text}</label>
             {formField}
-        </>
+        </div>
     )
 
 }
--- pages/form.js
+++ pages/form.js
@@ -1,13 +1,42 @@
 import Link from 'next/link'
 import Button from '../components/button'
 import FormField from '../components/formField'
+import { useState } from 'react'
+import supabase from '../utils/supabase'
 import styles from '../styles/form.module.css'
 
-function submitForm() {
+export async function getStaticProps() {
+    const { data: products, error } = await supabase.from('products').select('*').order('product_id', 'asc');
 
+    if (error) {
+        console.log(error);
+    }
+
+    return {
+        props: {
+            products,
+        }
+    }
 }
 
-export default function Form() {
+
+export default function Form({products}) {
+    const [formData, updateFormData] = useState();
+
+    const handleChange = (e) => {
+        updateFormData({
+            ...formData,
+            [e.target.name]: e.target.value.trim(),
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(formData);
+        //call function to insert order into orders table
+    }
+
+
     return (
         <div>
             <h1 className={styles.title}>Form</h1>
@@ -17,14 +46,15 @@ export default function Form() {
                 </Link>
             </h2>
             <form>
-                <div style={{display: 'flex', flexDirection: 'column'}}>
-                    <FormField type="input" text="Name" placeholder="Enter your name" required={true}/>
-                    <FormField type="input"  text="Email" placeholder="Enter your email" required={true}/>
-                    <FormField type="input" text="Phone" placeholder="Enter your phone" required={true}/>
-                    <FormField type="select" options={['Mississauga', 'Burlington']} text="City" placeholder="Enter your city" required={true}/>
-                    <FormField type="input" text="Address" placeholder="Enter your address" required={true}/>
-                    <FormField type="input" text="Additional Information" placeholder="Enter your any additional information" />
-                    <Button type="primary" clickHandler={submitForm}>Submit</Button>
+                <div style={{display: 'grid', gridTemplateColumns: '1fr 1fr 1fr'}}>
+                    <FormField onChange={handleChange} type="input" text="Name" placeholder="Enter your name" name="name" required={true}/>
+                    <FormField onChange={handleChange} type="input"  text="Email" placeholder="Enter your email" name="email" required={true}/>
+                    <FormField onChange={handleChange} type="input" text="Phone" placeholder="Enter your phone" name="phone" required={true}/>
+                    <FormField onChange={handleChange} type="select" options={['Mississauga', 'Burlington']} text="City" placeholder="Enter your city" name="city" required={true}/>
+                    <FormField onChange={handleChange} type="input" text="Address" placeholder="Enter your address" name="address" required={true}/>
+                    <FormField onChange={handleChange} type="input" text="Additional Information" placeholder="Enter any additional information" name="additional-information"/>
+                    {products.map(product => <FormField onChange={handleChange} type="input" text={product.product_name} placeholder="How many?" name={product.product_id} key={product.product_id}/>)}
+                    <Button type="primary" clickHandler={handleSubmit}>Submit</Button>
                 </div>
             </form>
         </div>
