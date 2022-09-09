0c6803c287666a254cf132bef278c45e002775b7
Christopher Oka <70914858+ChristopherOka@users.noreply.github.com>
2022-09-08 23:08:32 -0400

    finished image carosel

--- components/ImageCarosel.js
+++ components/ImageCarosel.js
@@ -1,31 +1,42 @@
 import Image from "next/image";
-import { useState, useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 export default function ImageCarosel(props) {
-    const [selected, setSelected] = useState(props.images[0]);
+    const selected = useRef(props.images[0]);
     const THREE_HALVES_IMAGE_WIDTH = 900;
-    let mounted = false;
+    const mounted = useRef(false);
 
     function swapImages(direction) {
         const caroselStart = Math.abs(
             THREE_HALVES_IMAGE_WIDTH -
                 document.getElementById("carosel").offsetWidth / 2
         );
-        const imageIdx = props.images.indexOf(selected);
-        const newIdx =
-            imageIdx + direction > 0
-                ? imageIdx + direction
-                : props.images.length - 1;
+        const imageIdx = props.images.indexOf(selected.current);
+        let newIdx = imageIdx + direction;
+        if (imageIdx + direction < 0) {
+            newIdx = props.images.length - 1;
+        } else if (imageIdx + direction >= props.images.length) {
+            newIdx = 0;
+        }
         const caroselImages = document.querySelector(".carosel-images");
         caroselImages.style.transform = `translateX(-${
             caroselStart + newIdx * 604
         }px)`;
         console.log(newIdx);
-        setSelected(props.images[newIdx]);
+        selected.current = props.images[newIdx];
+        document.querySelectorAll(".carosel-image.selected").forEach((el) => {
+            el.classList.remove("selected");
+            el.style = "opacity: 0.5";
+        });
+        const selectedImage = document.querySelector(
+            `#carosel-image-${newIdx}`
+        );
+        selectedImage.style = "opacity: 1";
+        selectedImage.classList.add("selected");
     }
 
     useEffect(() => {
-        if (mounted) return;
+        if (mounted.current) return;
         const caroselStart = Math.abs(
             THREE_HALVES_IMAGE_WIDTH -
                 document.getElementById("carosel").offsetWidth / 2
@@ -37,18 +48,21 @@ export default function ImageCarosel(props) {
         (function loopThroughImages() {
             setTimeout(() => {
                 swapImages(1);
-                if (props.images.indexOf(selected) < props.images.length) {
+                if (
+                    props.images.indexOf(selected.current) < props.images.length
+                ) {
                     loopThroughImages();
                 }
             }, 4000);
         })();
-        mounted = true;
+
+        mounted.current = true;
     }, []);
 
     return (
         <section className="flex justify-center items-center py-16">
             <div
-                className="flex w-[66vw] overflow-hidden relative"
+                className="flex w-[66vw] overflow-hidden relative group"
                 id="carosel"
             >
                 <div className="flex gap-1 carosel-images transition ease duration-700">
@@ -62,15 +76,14 @@ export default function ImageCarosel(props) {
                             objectFit="cover"
                         />
                     </div>
-                    {props.images.map((image) => {
-                        const isSelected = image === selected;
-
+                    {props.images.map((image, index) => {
                         return (
                             <div
                                 className={`w-[600px] h-[600px] carosel-image transition ease duration-700 ${
-                                    isSelected ? "" : "opacity-50"
+                                    index === 0 ? "selected" : "opacity-50"
                                 }`}
                                 key={image}
+                                id={`carosel-image-${index}`}
                             >
                                 <Image
                                     src={image}
@@ -92,8 +105,13 @@ export default function ImageCarosel(props) {
                         />
                     </div>
                 </div>
-                <div className="absolute flex justify-center items-center left-0 h-full">
-                    <button className="px-3 py-5 opacity-70 bg-black flex">
+                <div className="absolute flex opacity-0 justify-center items-center left-0 h-full transition-opacity group-hover:opacity-100">
+                    <button
+                        className="px-3 py-5 opacity-70 bg-black flex"
+                        onClick={() => {
+                            swapImages(-1);
+                        }}
+                    >
                         <Image
                             src="/images/icons/carosel_arrow.svg"
                             height="30"
@@ -101,7 +119,7 @@ export default function ImageCarosel(props) {
                         />
                     </button>
                 </div>
-                <div className="absolute flex justify-center items-center right-0 h-full">
+                <div className="absolute flex opacity-0 justify-center items-center right-0 h-full transition-opacity group-hover:opacity-100">
                     <button
                         className="px-3 py-5 opacity-70 bg-black flex rotate-180"
                         onClick={() => {
