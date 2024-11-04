94ec43bfd42a11e4f4ca0a6d163e6a1e44d88aa4
Wygo Dev <dev@wygo.world>
2024-11-03 19:13:23 -0500

    fixed print layout

--- components/OrderSummaryTable.js
+++ components/OrderSummaryTable.js
@@ -3,20 +3,20 @@ export default function OrderSummaryTable(props) {
         <div className="overflow-auto print:overflow-hidden w-full">
             <table className="rounded-md">
                 <thead>
-                    <tr className="text-slate-600 top-0 left-0 z-10 sticky text-left">
-                        <th className="font-medium pb-2 pr-12 pl-6 print:pl-0">
+                    <tr className="text-slate-600 top-0 left-0 z-10 sticky text-left [&>th]:print:bg-transparent print:!bg-transparent">
+                        <th className="font-medium pb-2 pr-12 pl-6 print:pl-0 print:pr-6">
                             Product Name
                         </th>
-                        <th className="font-medium pb-2 pr-12 text-center">
+                        <th className="font-medium pb-2 pr-12 text-center print:pr-6">
                             Total Dozens
                         </th>
-                        <th className="font-medium pb-2 pr-12 text-center">
+                        <th className="font-medium pb-2 pr-12 text-center print:pr-6">
                             Total Items
                         </th>
-                        <th className="font-medium pb-2 pr-12 text-center">
+                        <th className="font-medium pb-2 pr-12 text-center print:pr-6">
                             Number of Bags
                         </th>
-                        <th className="font-medium pb-2 pr-12 text-center">
+                        <th className="font-medium pb-2 pr-12 text-center print:pr-0">
                             Cost
                         </th>
                     </tr>
@@ -26,12 +26,12 @@ export default function OrderSummaryTable(props) {
                         return (
                             <tr
                                 key={product.product_id}
-                                className={`text-default-900 relative text-md print:border-t`}
+                                className={`text-default-900 relative text-md print:border-t print:!bg-transparent [&>td]:print:!bg-transparent`}
                             >
-                                <td className="py-2 pr-10 top-0 left-0 sticky md:pr-12 pl-6 font-medium print:pl-0">
+                                <td className="py-2 pr-10 top-0 left-0 sticky md:pr-12 pl-6 font-medium print:pl-0  print:pr-6">
                                     {product.product_name}
                                 </td>
-                                <td className="pr-12 font-bold text-center print:text-start">
+                                <td className="pr-12 font-bold text-center print:text-start print:pr-6">
                                     {product.measured_per != 1 ? (
                                         <>
                                             <span className="rounded-xl bg-teal-100 px-3 w-fit print:mr-5 border border-slate-500 print:px-0 print:border-none print:bg-transparent">
@@ -41,13 +41,13 @@ export default function OrderSummaryTable(props) {
                                                     parseFloat(
                                                         product.additional_total_dozens ||
                                                             0
-                                                    ) || "-"}
+                                                    ) || '-'}
                                             </span>
                                             <span className="hidden rounded-xl bg-white px-5 py-3 border-slate-400 border float-right print:inline"></span>
                                         </>
                                     ) : null}
                                 </td>
-                                <td className="pr-12 font-bold text-center print:text-start">
+                                <td className="pr-12 font-bold text-center print:text-start print:pr-6">
                                     <span className="rounded-xl bg-yellow-100 border border-slate-500 px-3 w-fit print:mr-5 print:px-0 print:border-none print:bg-transparent">
                                         {parseFloat(
                                             product.total_quantity || 0
@@ -55,11 +55,11 @@ export default function OrderSummaryTable(props) {
                                             parseFloat(
                                                 product.additional_total_quantity ||
                                                     0
-                                            ) || "-"}
+                                            ) || '-'}
                                     </span>
                                     <span className="hidden rounded-xl bg-white px-5 py-3 border-slate-400 border float-right print:inline"></span>
                                 </td>
-                                <td className="pr-12 font-bold text-center print:text-start">
+                                <td className="pr-12 font-bold text-center print:text-start print:pr-6">
                                     {product.measured_per != 1 ? (
                                         <span className="rounded-xl bg-pink-100 border border-slate-500 px-3 w-fit print:mr-5 print:px-0 print:border-none print:bg-transparent">
                                             {parseFloat(
@@ -70,47 +70,47 @@ export default function OrderSummaryTable(props) {
 
                                     <span className="hidden rounded-xl bg-white px-5 py-3 border-slate-400 border float-right print:inline"></span>
                                 </td>
-                                <td className="pr-12 font-bold text-center print:text-start">
-                                    <span className="rounded-xl bg-green-100 px-3 w-fit border border-slate-500 print:mr-5 print:px-0 print:border-none print:bg-transparent">
+                                <td className="pr-12 font-bold text-center print:text-start print:pr-0">
+                                    <span className="rounded-xl bg-green-100 px-3 w-fit border border-slate-500 print:pr-0 print:px-0 print:border-none print:bg-transparent">
                                         $
                                         {product.cost *
                                             (product.total_dozens ||
-                                                product.total_quantity) || "-"}
+                                                product.total_quantity) || '-'}
                                     </span>
                                     <span className="hidden rounded-xl bg-white px-5 py-3 border-slate-400 border float-right print:inline"></span>
                                 </td>
                             </tr>
-                        );
+                        )
                     })}
-                    <tr className={`text-default-900 text-md print:border-t`}>
-                        <td className="font-bold py-2 pr-10 md:pr-12 pl-6 print:pl-0">
+                    <tr className={`text-default-900 text-md print:border-t print:!bg-transparent [&>td]:print:!bg-transparent`}>
+                        <td className="font-bold py-2 pr-10 md:pr-12 pl-6 print:pl-0 print:pr-6">
                             Totals
                         </td>
-                        <td className="pr-12 font-bold text-center print:text-start">
+                        <td className="pr-12 font-bold text-center print:text-start print:pr-6">
                             <span className="rounded-xl bg-teal-100 px-3 w-fit border-slate-500 border print:mr-5 print:px-0 print:border-none print:bg-transparent">
                                 {props.products.reduce((total, product) => {
-                                    return total + product.total_dozens;
+                                    return total + product.total_dozens
                                 }, 0)}
                             </span>
                             <span className="hidden rounded-xl bg-white px-5 py-3 border-slate-400 border float-right print:inline"></span>
                         </td>
-                        <td className="pr-12 font-bold text-center print:text-start">
+                        <td className="pr-12 font-bold text-center print:text-start print:pr-6">
                             <span className="rounded-xl bg-yellow-100 px-3 w-fit border border-slate-500 print:mr-5 print:px-0 print:border-none print:bg-transparent">
                                 {props.products.reduce((total, product) => {
-                                    return total + product.total_quantity;
+                                    return total + product.total_quantity
                                 }, 0)}
                             </span>
                             <span className="hidden rounded-xl bg-white px-5 py-3 border-slate-400 border float-right print:inline"></span>
                         </td>
-                        <td className="pr-12 font-bold text-center print:text-start">
+                        <td className="pr-12 font-bold text-center print:text-start print:pr-6">
                             <span className="rounded-xl bg-pink-100 px-3 w-fit print:mr-5 print:px-0 border border-slate-500 print:border-none print:bg-transparent">
                                 {props.products.reduce((total, product) => {
-                                    return total + product.total_dozens;
+                                    return total + product.total_dozens
                                 }, 0)}
                             </span>
                             <span className="hidden rounded-xl bg-white px-5 py-3 border-slate-400 border float-right print:inline"></span>
                         </td>
-                        <td className="pr-12 font-bold text-center print:text-start">
+                        <td className="pr-12 font-bold text-center print:text-start print:pr-0">
                             <span className="rounded-xl bg-green-100 px-3 w-fit border border-slate-500 print:mr-5 print:px-0 print:border-none print:bg-transparent">
                                 $
                                 {props.products.reduce((total, product) => {
@@ -119,7 +119,7 @@ export default function OrderSummaryTable(props) {
                                         product.cost *
                                             (product.total_dozens ||
                                                 product.total_quantity)
-                                    );
+                                    )
                                 }, 0)}
                             </span>
                             <span className="hidden rounded-xl bg-white px-5 py-3 border-slate-400 border float-right print:inline"></span>
@@ -128,5 +128,5 @@ export default function OrderSummaryTable(props) {
                 </tbody>
             </table>
         </div>
-    );
+    )
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,9 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
     render() {
         return (
-            <Html className="bg-default-100 font-source-sans-pro">
+            <Html className="bg-default-100 font-source-sans-pro print:bg-transparent">
                 <Head>
                     <link
                         href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400;700&family=Fjalla+One"
@@ -15,8 +15,8 @@ class MyDocument extends Document {
                     <NextScript />
                 </body>
             </Html>
-        );
+        )
     }
 }
 
-export default MyDocument;
+export default MyDocument
