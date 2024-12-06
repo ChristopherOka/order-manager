e76ee4629432d43684cd954eb1fe4da10fe94dc2
Christopher Oka <chriskroka@gmail.com>
2024-12-06 16:02:03 -0500

    disabled december 12th

--- utils/constants.js
+++ utils/constants.js
@@ -9,7 +9,7 @@ export const DATE_RANGES = {
   "12th": {
     start_date: new Date("2024-12-07 00:00:00"),
     end_date: new Date("2024-12-12 00:00:00"),
-    disabled: false,
+    disabled: true,
   },
   "19th": {
     start_date: new Date("2024-12-12 00:00:00"),
