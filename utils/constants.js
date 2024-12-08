ec653cb7f15cb117450fd6b8ddcd7affe5bed87b
Christopher Oka <chriskroka@gmail.com>
2024-12-08 10:15:58 -0500

    closed orders for the season

--- utils/constants.js
+++ utils/constants.js
@@ -1,4 +1,4 @@
-export const HIDE_PRODUCTS = false;
+export const HIDE_PRODUCTS = true;
 export const CURRENT_SEASON = "2024";
 export const DATE_RANGES = {
   "7th": {
@@ -14,6 +14,6 @@ export const DATE_RANGES = {
   "19th": {
     start_date: new Date("2024-12-12 00:00:00"),
     end_date: new Date("2024-12-19 00:00:00"),
-    disabled: false,
+    disabled: true,
   },
 };
